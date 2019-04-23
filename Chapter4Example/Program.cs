6c0a9764487eb35ec347560cf0be5b85a8bd959d
ethanbrown3 <25070760+ethanbrown3@users.noreply.github.com>
2019-04-22 22:38:18 -0600

    Added balance actions

--- Chapter4Example/Program.cs
+++ Chapter4Example/Program.cs
@@ -10,16 +10,20 @@ namespace Chapter4Example {
             // create an Account object and assign it to myAccount
             Account myAccount = new Account("John Doe", 0.0m);
 
-            // display myAccount's initial name (there isn't one yet);
             Console.WriteLine($"Initial name is: {myAccount.Name}");
+            Console.WriteLine($"Initial balance is: {myAccount.Balance}");
 
             // prompt for and read the name then put the name in the object
             Console.Write("Enter the name: "); // prompt
             string theName = Console.ReadLine(); // read the name
             myAccount.Name = theName; // put theName in the myAccount object
+            Console.WriteLine("Enter Deposit Amount: ");
+            decimal theAmount = decimal.Parse(Console.ReadLine());
+            myAccount.Deposit(theAmount);
 
             // display the name stored in the myAccount object
             Console.WriteLine($"myAccount's name is: {myAccount.Name}");
+            Console.WriteLine($"myAccount's Balance is: {myAccount.Balance}");
             Console.ReadKey();
         }
     }
